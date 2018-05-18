3bd26b229e261737e0f4d6ff840760cda380b510
qiuwenchang <qiuwenchang@bytedance.com>
2018-05-18 17:36:12 +0800

    convert2python2

--- paddle/run_zhidao.sh
+++ paddle/run_zhidao.sh
@@ -49,9 +49,9 @@ vocab_size=200000
 
 train() {
     cp *.py $env_dir/
-    PYTHONPATH=$PWD:$ROOT CUDA_VISIBLE_DEVICES=0 python $env_dir/run.py \
+    PYTHONPATH=$PWD:$ROOT CUDA_VISIBLE_DEVICES=0 python2 $env_dir/run.py \
         --trainset /mnt/datasets/dureader-1st-round/preprocessed/trainset/zhidao.train.json \
-        --testset /mnt/datasets/dureader-1st-round/preprocessed/devtset/zhidao.dev.json \
+        --testset /mnt/datasets/dureader-1st-round/preprocessed/devset/zhidao.dev.json \
         --vocab_file /home/kesci/work/zhidao.vocab \
         --emb_dim $emb_dim \
         --batch_size 32 \
